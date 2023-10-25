fa0c0c7ff6c1bfe7c1b9f43006ae85faf2a3f4a6
lumeixu1 <meixvan.lu@mail.utoronto.ca>
2023-10-25 00:46:39 -0400

    finish the first draft of google authentication back end

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#index' #for login
+end
